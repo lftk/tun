1ee85a3dd883164c53264107251d8d4e44aa5af9
zhoujun <dev.zhoujun@gmail.com>
2017-07-04 19:38:54 +0800

    The client preferentially uses cmd config

--- cmd/tunc/main.go
+++ cmd/tunc/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"io/ioutil"
+	"os"
 	"time"
 
 	"gopkg.in/yaml.v2"
@@ -13,12 +14,14 @@ import (
 	"github.com/4396/tun/version"
 )
 
+type proxy struct {
+	Addr  string
+	Token string
+}
+
 type config struct {
 	Server  string
-	Proxies map[string]struct {
-		Addr  string
-		Token string
-	}
+	Proxies map[string]proxy
 }
 
 func loadConfig(path string) (cfg config, err error) {
@@ -31,15 +34,44 @@ func loadConfig(path string) (cfg config, err error) {
 	return
 }
 
+func LoadConfig() (cfg config, err error) {
+	_, errSt := os.Stat(*conf)
+	if errSt == nil {
+		cfg, err = loadConfig(*conf)
+		if err != nil {
+			return
+		}
+	}
+
+	if *server != "" {
+		cfg.Server = *server
+	}
+
+	if *name != "" && *addr != "" {
+		if cfg.Proxies == nil {
+			cfg.Proxies = make(map[string]proxy)
+		}
+		cfg.Proxies[*name] = proxy{
+			Addr:  *addr,
+			Token: *token,
+		}
+	}
+	return
+}
+
 var (
-	conf = flag.String("c", "conf/tunc.yaml", "config file's path")
+	conf   = flag.String("c", "conf/tunc.yaml", "config file's path")
+	server = flag.String("server", "", "tun server addr")
+	name   = flag.String("name", "", "tun proxy name")
+	token  = flag.String("token", "", "tun proxy token")
+	addr   = flag.String("addr", "", "tun proxy addr")
 )
 
 func main() {
 	flag.Parse()
 	log.Infof("Start tun client, version is %s", version.Version)
 
-	cfg, err := loadConfig(*conf)
+	cfg, err := LoadConfig()
 	if err != nil {
 		log.Fatalf("Load config failed, %v", err)
 		return
